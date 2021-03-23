1ca8a32174691d7bcd28161af64cd0c5d3bdc57b
Lindsay Wardell <three060@gmail.com>
2021-03-23 10:21:24 -0700

    added types to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,47 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
 import Admin from "../views/Admin.vue";
 import Login from "../views/Login.vue";
 
-const routes = [
+const routes: RouteRecordRaw[] = [
   {
     path: "/",
     name: "Home",
     component: Home,
+    beforeEnter(to, from, next) {
+      const loggedIn = localStorage.getItem("js-marathon");
+      if (!loggedIn) {
+        next({ path: "/login" });
+      } else {
+        next();
+      }
+    },
   },
   {
     path: "/admin",
     name: "Admin",
     component: Admin,
+    beforeEnter(to, from, next) {
+      const loggedIn = localStorage.getItem("js-marathon");
+      if (!loggedIn) {
+        next({ path: "/login" });
+      } else {
+        next();
+      }
+    },
   },
   {
     path: "/login",
     name: "Login",
     component: Login,
+    beforeEnter(to, from, next) {
+      const loggedIn = localStorage.getItem("js-marathon");
+      if (loggedIn) {
+        next({ path: "/" });
+      } else {
+        next();
+      }
+    },
   },
 ];
 

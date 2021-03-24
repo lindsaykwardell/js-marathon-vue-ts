948695e1c10895c9c8c379b6e7de41b9aed0cc77
Lindsay Wardell <three060@gmail.com>
2021-03-23 18:47:24 -0700

    updated tests

--- cypress/integration/post-typescript.js
+++ cypress/integration/post-typescript.js
@@ -1,17 +1,9 @@
-describe("Test Runner", () => {
-  beforeEach(() => {
-    sessionStorage.setItem("js-marathon", "username");
-  })
-
-  it("runs tests", () => {
-    cy.visit(Cypress.config().baseUrl + "/");
-    cy.get("ul > li").should("have.length", 6);
-    cy.findByText("Start Tests").click();
-    cy.findByText("All tests have completed!").should("exist");
-  });
-
+describe("Test Runner post-typescript", () => {
   it("allows adding new tests", () => {
     cy.visit(Cypress.config().baseUrl + "/");
+    cy.url().should("include", "/login");
+    cy.get("input").click().type("username");
+    cy.get("button").click();
     cy.get("nav").findByText("Admin Panel").click();
     cy.get("ul > li").should("have.length", 6);
     cy.get("input").click().type("A new test approaches.");
